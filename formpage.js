fdf61e57e0e7343bb377f39695ef3ed16760a3cf
James <Jamesj@missionreadyhq.com>
2026-01-08 11:42:49 +1300

    Added Javascript file and media query in CSS

---
+++ formpage.js
@@ -0,0 +1,14 @@
+// Bootstrap form validation (keep your existing validation)
+(function () {
+    'use strict'
+    var form = document.querySelector('.needs-validation')
+
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+    }, false)
+})();
+
